4219b66fc8598cb7f6a0f6373a21aafaaf4332fb
seaniezhao <zwxbest@hotmail.com>
2016-07-02 18:57:20 +0800

    fix
    fix

---
+++ Assets/Scripts/utility/Asset.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using Object=UnityEngine.Object;
+namespace LegoVR
+{
+    public class Asset:IDisposable
+    {
+        public string AssetName { get; set; }
+        public string AssetPath { get; set; }
+        public delegate void AssetLoaded(Asset arg);
+        public event AssetLoaded OnAssetLoaded;
+
+        //是否加载成功
+        public bool IsDone = false;
+
+        //实际资源对象
+        private Object _realAsset;
+
+
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
